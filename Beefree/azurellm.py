2c57dde0fc6507c694a47b2df556e4f000f2b4e0
Sumanth Gudla <sumanthg@Sumanths-MacBook-Air.local>
2026-08-15 15:15:41 +0530

    beefree changes

--- Beefree/azurellm.py
+++ Beefree/azurellm.py
@@ -1,8 +1,19 @@
 from langchain_openai import AzureChatOpenAI
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+KEY_VAULT_URL='https://key-vault-azure12.vault.azure.net/'
+credential = DefaultAzureCredential()
+secret_client = SecretClient(
+    vault_url=KEY_VAULT_URL,
+    credential=credential
+)
 def getllm():
+    api_key = secret_client.get_secret(
+        "my-ai-keyvault"
+    ).value
     return AzureChatOpenAI(
     azure_deployment='gpt-4.1',
     azure_endpoint='https://learning468.services.ai.azure.com/',
-    api_key=os.getenv("azure_api_key"),
+    api_key=api_key,
     api_version='2025-04-01-preview'
 )
--- Beefree/nodes/validate_schema.py
+++ Beefree/nodes/validate_schema.py
@@ -2,16 +2,5 @@ import json
 from pathlib import Path
 from state import BeefreeState
 def validate(state: BeefreeState):
-    json1=state.simple_schema
-    Allowed_list=('heading','paragraph')
-    data=json.loads(json1)
-    template=data['template']
-    rows=template['rows']
-    for rowidx,row in enumerate(rows):
-        coulms=row['columns']
-        for colidx,col in enumerate(coulms):
-            modules=col['modules']
-            for modix,mod in enumerate(modules):
-                if mod['type'].lower() in Allowed_list:
-                    print(mod['type'])
+    
     return {'is_validated':True}

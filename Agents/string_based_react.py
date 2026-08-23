a6cb32b1d60988491b4c99d374759c2333c88614
Sumanth Gudla <sumanthg@Sumanths-MacBook-Air.local>
2026-08-23 20:08:40 +0530

    removed keys

--- Agents/string_based_react.py
+++ Agents/string_based_react.py
@@ -8,7 +8,7 @@ api_key=os.getenv('api_key')
 llm=AzureChatOpenAI(
     azure_deployment='gpt-4.1',
     azure_endpoint='https://learning468.services.ai.azure.com/',
-    api_key=os.getenv("azure_api_key"),
+    api_key='',
     api_version='2025-04-01-preview'
 )
 
